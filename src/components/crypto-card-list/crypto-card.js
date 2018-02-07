f3e2f041f1fce3be901c2ddb7394303ad54ea4a9
Hyeseong Kim <cometkim.kr@gmail.com>
2018-02-07 22:50:39 +0900

    Cleanup CryptoCard component using styled-components

--- src/components/crypto-card-list/crypto-card.js
+++ src/components/crypto-card-list/crypto-card.js
@@ -1,33 +1,29 @@
 import React from 'react'
-import { Image, Text, View, StyleSheet } from 'react-native'
+import { Text } from 'react-native'
+import styled from 'styled-components/native'
 
 import { coinIcons } from 'utils/constants'
 
-const CryptoCard = ({
+export default ({
     symbol,
-    coin_name,
     price_usd,
     percent_change_24h,
     percent_change_7d,
 }) => (
-    <View style={styles.container}>
-        <Image style={styles.image} source={{ uri: coinIcons[symbol] }} />
+    <Container>
+        <IconImage source={{ uri: coinIcons[symbol] }} />
         <Text>{symbol}</Text>
-        <Text>{coin_name}</Text>
-        <Text>{price_usd} $</Text>
-        <Text>Chnage past 24 hours: {percent_change_24h}</Text>
-        <Text>Change past 7 days: {percent_change_7d}</Text>
-    </View>
+        <Text>$ {price_usd}</Text>
+        <Text>Chnage past 24 hours: {percent_change_24h}%</Text>
+        <Text>Change past 7 days: {percent_change_7d}%</Text>
+    </Container>
 )
 
-const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-    },
-    image: {
-        width: 40,
-        height: 40,
-    },
-})
+const Container = styled.View`
+    display: flex;
+`
 
-export default CryptoCard
+const IconImage = styled.Image`
+    width: 40;
+    height: 40;
+`
